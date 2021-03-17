8553a99f81a9a131082fd3218e978a1b85c8d49a
yutian.liu <939029835@qq.com>
2021-03-17 16:46:46 +0800

    新增父节点属性支持、修复bind无效时会卡死的问题

--- StageCanvas/Export/html5/haxe/ApplicationMain.hx
+++ StageCanvas/Export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "38";
+		app.meta["build"] = "40";
 		app.meta["company"] = "Company Name";
 		app.meta["file"] = "ZIde-UI";
 		app.meta["name"] = "ZIde";
