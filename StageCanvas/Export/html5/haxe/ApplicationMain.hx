155b6fb4a676c05200060a22359417cfadfd089a
yutian.liu <939029835@qq.com>
2021-05-13 09:07:41 +0800

    新增忽略文件

--- StageCanvas/Export/html5/haxe/ApplicationMain.hx
+++ StageCanvas/Export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "46";
+		app.meta["build"] = "47";
 		app.meta["company"] = "Company Name";
 		app.meta["file"] = "ZIde-UI";
 		app.meta["name"] = "ZIde";
