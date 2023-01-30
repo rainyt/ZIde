deb26015f02a957a526ee09acc7b862482141d88
Rainy <939029835@qq.com>
2023-01-30 09:55:42 +0800

    更新配置，以及删除Export

--- atlas-tools/AutoExportFnt.hx
+++ atlas-tools/AutoExportFnt.hx
@@ -8,62 +8,66 @@ import openfl.display.Bitmap;
 import sys.io.File;
 
 class AutoExportFnt {
-    
-    public static function createFnt(path:String,bitmapData:BitmapData = null):Fnt{
-        return new Fnt(path,StringTools.replace(path, ".png", ".fnt"),bitmapData);
-    }
-
+	public static function createFnt(path:String, bitmapData:BitmapData = null):Fnt {
+		return new Fnt(path, StringTools.replace(path, ".png", ".fnt"), bitmapData);
+	}
 }
 
 class Fnt {
+	public var maps:Map<String, BitmapData> = [];
 
-    public var maps:Map<String,BitmapData> = [];
-    
-    public function new(file:String,fnt:String,bitmapData:BitmapData) {
-        var fntContent:UTF8String = File.getContent(fnt);
-        trace("字列表：",fntContent);
-        var bitmapData = bitmapData != null?bitmapData:BitmapData.fromFile(file);
-        // 是否开始剪切
-        var isStartCut:Bool = false;
-        var ix:Int = 0;
-        // 字的索引
-        var index:Int = 0;
-        // 开始剪切的位置
-        var cutX:Int = 0;
-        // 结束剪切的位置
-        var endX:Int = 0;
-        // 从左侧开始计算
+	public function new(file:String, fnt:String, bitmapData:BitmapData) {
+		var fntContent:UTF8String = File.getContent(fnt);
+		trace("字列表：", fntContent);
+		var bitmapData = bitmapData != null ? bitmapData : BitmapData.fromFile(file);
+		// 是否开始剪切
+		var isStartCut:Bool = false;
+		var ix:Int = 0;
+		// 字的索引
+		var index:Int = 0;
+		// 开始剪切的位置
+		var cutX:Int = 0;
+		// 结束剪切的位置
+		var endX:Int = 0;
+		// 从左侧开始计算
 		while (ix < bitmapData.width) {
-            // trace(bitmapData.width - 1,ix,isStartCut);
+			// trace(bitmapData.width - 1,ix,isStartCut);
 			if (!isAlpha(bitmapData.getPixels(new Rectangle(ix, 0, 1, bitmapData.height))) && ix != bitmapData.width - 1) {
-				if(!isStartCut){
-                    //开始剪切
-                    isStartCut = true;
-                    cutX = ix;
-                }
-            }
-            else if(isStartCut)
-            {
-                //透明处理
-                isStartCut = false;
-                endX = ix;
-                //开始保存
-                var fntBitmapData = new BitmapData(endX - cutX,bitmapData.height,true,0x0);
-                fntBitmapData.copyPixels(bitmapData,new Rectangle(cutX,0,endX - cutX,bitmapData.height),new Point());
-                maps.set(Utf8.sub(fntContent,index,1),fntBitmapData);
-                // trace("剪切结束，字是：",Utf8.sub(fntContent,index,1),cutX,endX);
-                index ++;
-            }
-            ix ++;
+				if (!isStartCut) {
+					// 开始剪切
+					isStartCut = true;
+					cutX = ix;
+				}
+			} else if (isStartCut) {
+				// 往右边检测10px，看看是否已经是透明，如果存在不透明的情况，则当做同一体来处理。
+				var testIsAlpha = true;
+				for (i in 0...10) {
+					if (!isAlpha(bitmapData.getPixels(new Rectangle(ix + i, 0, 1, bitmapData.height))) && ix + i != bitmapData.width - 1) {
+						testIsAlpha = false;
+						break;
+					}
+				}
+				if (testIsAlpha) {
+					// 透明处理
+					isStartCut = false;
+					endX = ix;
+					// 开始保存
+					var fntBitmapData = new BitmapData(endX - cutX, bitmapData.height, true, 0x0);
+					fntBitmapData.copyPixels(bitmapData, new Rectangle(cutX, 0, endX - cutX, bitmapData.height), new Point());
+					maps.set(Utf8.sub(fntContent, index, 1), fntBitmapData);
+					// trace("剪切结束，字是：",Utf8.sub(fntContent,index,1),cutX,endX);
+					index++;
+				}
+			}
+			ix++;
 		}
-    }
+	}
 
-    public static function isAlpha(array:ByteArray):Bool {
+	public static function isAlpha(array:ByteArray):Bool {
 		for (i in 0...array.length) {
 			if (array[i] != 0)
 				return false;
 		}
 		return true;
 	}
-
 }
