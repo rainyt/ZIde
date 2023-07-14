be73a1deae13ff7a9b36f0d2916a8cfbca434fce
Rainy <939029835@qq.com>
2023-07-15 00:13:37 +0800

    提交初步程序

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,3 +1,13 @@
+import element.plus.ElementPlus;
+import vue3.Vue;
+import app.App;
+
 class Main {
-	static function main() {}
+	static function main() {
+        trace("ZIde v2.0 inited");
+		var config = new App();
+		var app = Vue.createApp(config);
+        app.use(ElementPlus);
+		app.mount("#app");
+	}
 }
---
+++ Source/app/App.hx
@@ -0,0 +1,9 @@
+package app;
+
+import vue3.VueComponent;
+
+/**
+ * 应用主程序
+ */
+@:t("html/app.html")
+class App extends VueComponent {}
