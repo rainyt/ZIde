c2fa5ea45815bb57f0262b168e698d5cfa443d4d
Rainy <939029835@qq.com>
2023-07-15 20:16:35 +0800

    初步tabs支持

--- Source/app/App.hx
+++ Source/app/App.hx
@@ -17,19 +17,59 @@ class App extends VueComponent {
 		return {
 			filterFileName: "",
 			files: [],
-			tabs: [
-				{
-					title: 'Tab 1',
-					name: '1',
-					content: 'Tab 1 content',
-				},
-				{
-					title: 'Tab 2',
-					name: '2',
-					content: 'Tab 2 content',
-				},
-			]
+			tabKey: "",
+			tabs: [],
 		}
+	}
+
+	/**
+	 * 代码发生变更时触发
+	 */
+	public function onCodeChange(url:String, data:String):Void {
+		var tabData = this.getCurrentTapData();
+		if (tabData != null && tabData.name == url) {
+			var editer = this.get("editer", IFrameElement);
+			if (!tabData.isChange) {
+				tabData.isChange = tabData.code != untyped editer.contentWindow.getCodeValue();
+			}
+			tabData.code = untyped editer.contentWindow.getCodeValue();
+		}
+	}
+
+	/**
+	 * 清理没有修改过的Tab
+	 */
+	public function cleanNoChangeTab():Void {
+		var array:Array<TapData> = this.tabs;
+		array = array.filter((a) -> return a.name == tabKey || a.isChange);
+		this.tabs = array;
+	}
+
+	public function onTabChange(item):Void {
+		this.tabKey = item.props.name;
+		// 渲染到编辑器中
+		var editer = this.get("editer", IFrameElement);
+		untyped editer.contentWindow.setCodeValue(this.tabKey, getCurrentTapData().code);
+		this.onRender();
+	}
+
+	public function onRemoveTab(item):Void {
+		var array:Array<TapData> = this.tabs;
+		array = array.filter((a) -> a.name != item);
+		this.tabs = array;
+	}
+
+	public function addTap(item:TapData):Void {
+		var array:Array<TapData> = this.tabs;
+		if (array.filter((a) -> a.name == item.name).length == 0)
+			array.push(item);
+		this.tabs = array;
+	}
+
+	public function getCurrentTapData(?name:String):TapData {
+		name = name ?? this.tabKey;
+		var array:Array<TapData> = this.tabs;
+		return array.filter((a) -> a.name == name)[0];
 	}
 
 	public function onOpenProject():Void {
@@ -65,10 +105,23 @@ class App extends VueComponent {
 	}
 
 	/**
+	 * 将文件保存
+	 */
+	public function onSave():Void {
+		// TODO
+	}
+
+	/**
 	 * 开始渲染UI
 	 */
 	public function onRender():Void {
-		var uiediter = this.get("uiediter", IFrameElement);
+		var currentData = this.getCurrentTapData();
+		if (currentData != null) {
+			var editer = this.get("editer", IFrameElement);
+			var uiediter = this.get("uiediter", IFrameElement);
+			var code:String = untyped editer.contentWindow.getCodeValue();
+			untyped uiediter.contentWindow.openFile(currentData.path, code, AppData.currentProject, null);
+		}
 	}
 
 	/**
@@ -78,8 +131,26 @@ class App extends VueComponent {
 		var xmlContent = File.getContent(item.path);
 		// 渲染到编辑器中
 		var editer = this.get("editer", IFrameElement);
-		untyped editer.contentWindow.setCodeValue(xmlContent);
+		untyped editer.contentWindow.onCodeChange = this.onCodeChange;
+		untyped editer.contentWindow.setCodeValue(item.label, xmlContent);
+		this.addTap({
+			title: item.label,
+			path: item.path,
+			name: item.label,
+			code: xmlContent,
+			isChange: false
+		});
+		this.tabKey = item.label;
+		this.cleanNoChangeTab();
 		// 尝试渲染
 		onRender();
 	}
+}
+
+typedef TapData = {
+	title:String,
+	name:String,
+	path:String,
+	code:String,
+	isChange:Bool
 }
