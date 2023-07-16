43d8cb93a18efef11e6c81f3bcee10467182378a
Rainy <939029835@qq.com>
2023-07-16 10:01:51 +0800

    初步布局界面实现

--- Source/app/PsdExportView.hx
+++ Source/app/PsdExportView.hx
@@ -6,4 +6,24 @@ import vue3.VueComponent;
  * PSD导出界面实现
  */
 @:t("html/psd_export_view.html")
-class PsdExportView extends VueComponent {}
+class PsdExportView extends VueComponent {
+	override function data():Dynamic {
+		return {
+			option: {
+				psd: "",
+				atlas: true,
+				laytouFile: true,
+				batchRender: true,
+				s9out: true,
+				miniPng: true
+			}
+		}
+	}
+
+	/**
+	 * 将PSD格式导出处理
+	 */
+	public function onExport():Void {
+		// 开始导出
+	}
+}
